4a91e462378595e48d1f0e128f28ba2c50f8aff8
Jissan luna <teza2000@hotmail.com>
2020-02-26 12:26:28 -0700

    primer parcial

--- 02_login/api/login.php
+++ 02_login/api/login.php
@@ -1,40 +1,38 @@
 <?php
 require_once '../lib/myconnection.php';
 
-header('content-type: application/json')
-// Revisar el metodo utilizado en la peticion 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    if (isset($_POST['username']) && isset($isset($_POST{
+header('Content-Type: application/json');
+// Revisar el método utilizado en la petición
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['username']) && isset($_POST['userpasswd'])) {
         $username = $_POST['username'];
-        $userpassword = $_POST['userpasword'];
+        $userpassword = $_POST['userpasswd'];
 
-        //abrir una conexion 
-        $cnn = new myconnection();
-        //definir la consulta
-        $sql = printf("select name,email,firstname from users where name=%s'and
-        password=%s'and password= sha'"$username,$userpassword);
-        //ejecutar la consulta 
-        $rst =$cnn->query($sql);
-        //cerrar la conexion
-        $scn ->close();
-        //evaluar el resultado
-        if (!$rst){
-            // TODO: manejar el error de la ejecusion de la consulta
-            echo json_encode(['error'=> 'error al ejecutar la consulta'])
-        }elseif ($rst->num_rows == 1 {
-            // TODO: manejar la respuesta de la base de datos
-            // para preparar lo que se enviara al frontend
+        // abrir una conexion a mysql
+        $cnn = new MyConnection();
+        // definir la consulta
+        $sql = sprintf("select name,email,firstname,lastname from users where name='%s' and passwd=sha('%s')", $username, $userpassword);
+        // ejecutar la consulta
+        $rst = $cnn->query($sql);
+        // cerrar la conexion
+        $cnn->close();
+        // evaluar el resultado
+        if (!$rst) {
+            // manejar el error de la ejecución de la consulta
+            echo json_encode(['error' => 'Error al ejecutar la consulta: '.$sql]);
+        } elseif ($rst->num_rows == 1) {
+            // manejar la respuesta de la base de datos
+            // para preparar lo que se enviará al frontend
             // La respuesta esperada aqui es un objeto de tipo mysqli_result
-            $usuario = $rst ->fetch_assoc();
+            $usuario = $rst->fetch_assoc();
             echo json_encode(['data' => $usuario]);
-        }else {
-            echo json_encode(['respuesta' => 'usuario incorrecto.'])
+        } else {
+            echo json_encode(['respuesta' => 'Usuario incorrecto.']);
         }
+    } else {
+        echo json_encode(['error' => 'no se recibieron todos los valores']);
     }
-    echo json_encode(['usuario' => $username, 'contraseña' => $userpassword]);    
-}   else{
-    echo json_encode(['error' => 'no se recibieron valores']);
-}else {
+} else {
     http_response_code(405);
-    echo json_encode(['error' => 'el metodo'.$_SERVER['REQUEST_METHOD'].'NO ESTA PERMITIDO'])
+    echo json_encode(['error' => 'El método '.$_SERVER['REQUEST_METHOD'].' no esta permitido']);
 }
---
+++ frontend/js/app.js
@@ -0,0 +1,19 @@
+/** 
+ * javascrip para conectar ala API
+ * se requiere hacer peticiones ajax ala api
+ * http://localhost:8000/login.php
+ * y obtendra como resultado un JSON 
+ * 
+ * AJAX: asincronic javascrip AND XML
+*/
+
+fuction validarUsuario(){
+
+}
+
+$(document).ready(fuction (){
+    $('#btnlogin').click(fuction (e){
+        e.preventDefault();
+        alert("si hace algo");
+    })
+})
